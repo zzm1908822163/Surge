af33bcddd7beb4d8d1ed5ede35bb7b078cbc41ec
Alan <86471714+zzm1908822163@users.noreply.github.com>
2022-04-16 22:52:54 +0800

    Update

--- Scripts/Profile-Reload.js
+++ Scripts/Profile-Reload.js
@@ -1,64 +1,18 @@
-let params = getParams($argument)
+/*
+By：Pysta
+GitHub:https://github.com/mieqq/mieqq
+* [Panel]
+* 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
+* [Script]
+* 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
+*/
 
-!(async () => {
-/* 时间获取 */
-let traffic = (await httpAPI("/v1/traffic","GET"))
-let dateNow = new Date()
-let dateTime = Math.floor(traffic.startTime*1000)
-let startTime = timeTransform(dateNow,dateTime)
-let title = params.title
-
-
-if ($trigger == "button") await httpAPI("/v1/dns/flush");
-
-  $done({
-      title:title,
-      content:`启动时长: ${startTime}`,
-		icon: params.icon,
-		"icon-color":params.color
+$httpAPI("POST", "/v1/profiles/reload", {}, data => {
+    $notification.post("配置重载","配置重载成功","")
+    $done({
+        title: "配置重载",
+        content: "配置重载成功",
+        icon: "terminal",
+        "icon-color": "#5AC8FA",
+     })
     });
-
-})();
-
-function timeTransform(dateNow,dateTime) {
-let dateDiff = dateNow - dateTime;
-let days = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
-let leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
-let hours=Math.floor(leave1/(3600*1000))//计算出小时数
-//计算相差分钟数
-let leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
-let minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
-//计算相差秒数
-let leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
-let seconds=Math.round(leave3/1000)
-
-if(days==0){
-
-	if(hours==0){
-	if(minutes==0)return(`${seconds}秒`);
-	return(`${minutes}分${seconds}秒`)
-	}
-	return(`${hours}时${minutes}分${seconds}秒`)
-	}else {
-	return(`${days}天${hours}时${minutes}分`)
-	}
-
-}
-
-
-function httpAPI(path = "", method = "POST", body = null) {
-    return new Promise((resolve) => {
-        $httpAPI(method, path, body, (result) => {
-            resolve(result);
-        });
-    });
-}
-
-function getParams(param) {
-  return Object.fromEntries(
-    $argument
-      .split("&")
-      .map((item) => item.split("="))
-      .map(([k, v]) => [k, decodeURIComponent(v)])
-  );
-}
