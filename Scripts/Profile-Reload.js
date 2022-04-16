077eb5d0721798eeacbd0a662f7a2a6fde5b63d4
Alan <86471714+zzm1908822163@users.noreply.github.com>
2022-04-16 22:54:30 +0800

    Update

--- Scripts/Profile-Reload.js
+++ Scripts/Profile-Reload.js
@@ -1,10 +1,8 @@
-/*
-By：Pysta
-GitHub:https://github.com/mieqq/mieqq
+/* 
 * [Panel]
 * 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
 * [Script]
-* 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
+* 配置重载=script-path=https://raw.githubusercontent.com/zzm1908822163/Surge/master/Scripts/Profile-Reload.js,type=generic
 */
 
 $httpAPI("POST", "/v1/profiles/reload", {}, data => {
