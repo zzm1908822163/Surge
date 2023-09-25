fe52e68bf5dfb4342b6ee1d2594c28de8b9f3d20
Alan <86471714+zzm1908822163@users.noreply.github.com>
2023-09-26 06:40:28 +0800

    Update wifi-changed.js

--- Scripts/wifi-changed.js
+++ Scripts/wifi-changed.js
@@ -1,10 +1,7 @@
 const WIFI_DONT_NEED_PROXYS = ['ASUS_5G']; 
    if (WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)) { 
  $surge.setOutboundMode('direct'); 
- $('Surge', 'Wi-Fi changed', 'use direct mode'); 
  } else { 
  $surge.setSelectGroupPolicy('Final-select', 'Group'); 
- $surge.setOutboundMode('rule'); 
- $('Surge', 'Wi-Fi changed', 'use rule-based proxy mode'); 
- } 
+ $surge.setOutboundMode('rule');  } 
    $done(); 
