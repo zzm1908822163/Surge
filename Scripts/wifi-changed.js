8174fe6c17d53f7d38c8df68e7627adfc87d621f
Alan <86471714+zzm1908822163@users.noreply.github.com>
2023-09-26 06:35:11 +0800

    Update wifi-changed.js

--- Scripts/wifi-changed.js
+++ Scripts/wifi-changed.js
@@ -1,21 +1,10 @@
-const WIFI_DONT_NEED_PROXYS = [“ASUS_5G”];
-const CURRENT_WIFI_SSID_KEY = ‘current_wifi_ssid’;
-if (wifiChanged()) {
-const mode = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
-? ‘direct’
-: ‘rule’;
-$surge.setOutboundMode(mode);
-(
-‘Surge’,
-`Wi-Fi changed to ${$network.wifi.ssid || ‘cellular’}`,
-`use ${mode} mode`
-);
-}
-function wifiChanged() {
-const currentWifiSSid = $persistentStore.read(CURRENT_WIFI_SSID_KEY);
-const changed = currentWifiSSid !== $network.wifi.ssid;
-changed && $persistentStore.write($network.wifi.ssid,
-CURRENT_WIFI_SSID_KEY);
-return changed;
-}
-$done();
+const WIFI_DONT_NEED_PROXYS = ['ASUS_5G']; 
+   if (WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)) { 
+ $surge.setOutboundMode('direct'); 
+ $('Surge', 'Wi-Fi changed', 'use direct mode'); 
+ } else { 
+ $surge.setSelectGroupPolicy('Final-select', 'Group'); 
+ $surge.setOutboundMode('rule'); 
+ $('Surge', 'Wi-Fi changed', 'use rule-based proxy mode'); 
+ } 
+   $done(); 
