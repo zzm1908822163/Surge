a56ac8367278d9e3ab1d37da91aa2bd377131391
Alan <86471714+zzm1908822163@users.noreply.github.com>
2023-09-25 22:17:49 +0800

    Create WiFi-Changed.js

---
+++ Scripts/WiFi-Changed.js
@@ -0,0 +1,21 @@
+const WIFI_DONT_NEED_PROXYS = [“ASUS_5G”];
+const CURRENT_WIFI_SSID_KEY = ‘current_wifi_ssid’;
+if (wifiChanged()) {
+const mode = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
+? ‘direct’
+: ‘rule’;
+$surge.setOutboundMode(mode);
+(
+‘Surge’,
+`Wi-Fi changed to ${$network.wifi.ssid || ‘cellular’}`,
+`use ${mode} mode`
+);
+}
+function wifiChanged() {
+const currentWifiSSid = $persistentStore.read(CURRENT_WIFI_SSID_KEY);
+const changed = currentWifiSSid !== $network.wifi.ssid;
+changed && $persistentStore.write($network.wifi.ssid,
+CURRENT_WIFI_SSID_KEY);
+return changed;
+}
+$done();
